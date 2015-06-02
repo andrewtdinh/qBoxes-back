41605735bca6df3b1ecfb486d4a618250196c15c
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-02 02:05:04 -0700

    Add some test boxes in containers.json

--- scripts/clean-db.sh
+++ scripts/clean-db.sh
@@ -6,4 +6,4 @@ if [ -z "$1" ] ; then
 fi
 
 mongoimport --jsonArray --drop --db $1 --collection users --file ../db/users.json
-mongoimport --jsonArray --drop --db $1 --collection profiles --file ../db/profile.json
+mongoimport --jsonArray --drop --db $1 --collection containers --file ../db/containers.json
